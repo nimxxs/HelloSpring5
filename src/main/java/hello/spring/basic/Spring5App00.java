6c2279f4bd5d63f800f956d6a1469401480f8e07
nimxxs <tnals2199@naver.com>
2023-06-23 11:18:30 +0900

    README 추가

--- src/main/java/hello/spring/basic/Spring5App00.java
+++ src/main/java/hello/spring/basic/Spring5App00.java
@@ -4,6 +4,13 @@ public class Spring5App00 {
     // 첫 번째 사례
     // 간단한 인사말을 출력하는 프로그램 작성
     public static void main(String[] args) {
+        // sayHello 메서드를 호출하기 위해
+        // Spring5App00의 객체화 필요!
+        Spring5App00 app00 = new Spring5App00();
+        app00.sayHello();
+    }
+
+    private void sayHello() {
         System.out.println("Hello, World!!");
     }
 }
