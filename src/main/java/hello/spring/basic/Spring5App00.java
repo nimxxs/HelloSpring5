6686daa95852250501029823836bf17586e363ea
nimxxs <tnals2199@naver.com>
2023-06-23 11:04:57 +0900

    스프링 첫 번째 사례

---
+++ src/main/java/hello/spring/basic/Spring5App00.java
@@ -0,0 +1,9 @@
+package hello.spring.basic;
+
+public class Spring5App00 {
+    // 첫 번째 사례
+    // 간단한 인사말을 출력하는 프로그램 작성
+    public static void main(String[] args) {
+        System.out.println("Hello, World!!");
+    }
+}
